0104b5f9d111dae75eed36145014399416ca680a
Dev-moe-kyawaung <moekyawaung2027@gmail.com>
2026-04-23 03:45:27 +0630

    Add Footer component with copyright and GitHub link

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 မိုးကျော်အောင် | Portfolio Website</p>
+      <p>
+        GitHub: <a href="https://github.com/Dev-moe-kyawaung">Dev-moe-kyawaung</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
+
